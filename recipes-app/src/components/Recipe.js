ba7b8313a09c09303d322b1dc162d0688331ca4b
JMMFL <mehravarj+github@gmail.com>
2021-09-08 13:45:41 -0400

    Finished Chapter 5

--- recipes-app/src/components/Recipe.js
+++ recipes-app/src/components/Recipe.js
@@ -4,7 +4,7 @@ import Instructions from "./Instructions";
 
 export default function Recipe({ name, ingredients, steps }) {
     return (
-        <section id={name.toLowerCaste().replace(/ /g, "-")}>
+        <section id={name.toLowerCase().replace(/ /g, "-")}>
             <h1>{name}</h1>
             <IngredientsList list={ingredients} />
             <Instructions title="Cooking Instructions" steps={steps} />
