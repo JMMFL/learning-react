9493cf6f480f9e4dcb782ca0d1849803243d221d
JMMFL <mehravarj+github@gmail.com>
2021-10-14 14:42:13 -0400

    Added StarRating component from Chapter 6.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Chapter5 from "./chapters/Chapter5";
+import Chapter6 from "./chapters/Chapter6";
 
 function App() {
   return (
     <>
       <Chapter5 />
+      <Chapter6 />
     </>
   );
 }
---
+++ src/chapters/Chapter6.js
@@ -0,0 +1,7 @@
+import StarRating from '../components/StarRating';
+
+function Chapter6() {
+  return <StarRating />
+}
+
+export default Chapter6;
